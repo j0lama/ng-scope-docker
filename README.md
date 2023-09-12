dc662d7567a864b95920a9278002a53841cb6d3a
Jon Larrea <jlarreamartinez@ucsd.edu>
2023-09-12 16:17:51 -0700

    Multiple changes and features added

--- README.md
+++ README.md
@@ -2,9 +2,19 @@
 Utility for running NG-Scope using Docker
 
 # Run NG-Scope docker
-A precompiled NG-Scope docker image can be used to run NG-Scope anywhere. Use *j0lama/ng-scope:latest* and specify the EARFCN value: 
+A precompiled NG-Scope docker image can be used to run NG-Scope anywhere. Use **run.sh** to deploy it as follows: 
 ```bash
-./run.sh j0lama/ng-scope:latest <EARFCN List>
+Usage: ./ng-scope.sh [Options...]
+  -i, --image <Image name>        Name of the Docker image (e.g. j0lama/ng-scope:latest)
+  -o, --out  <Output folder>      Name of the Docker image (e.g. j0lama/ng-scope:latest)
+  -f, --frag                      Enable log fragmentation
+  -e, --earfcn "<EARFCN List>"    List of EARFCN (Use quotes)
+  -h, --help                      Show help menu
+Examples:
+  ./ng-scope.sh --image j0lama/ng-scope:latest --earfcn "700"
+  ./ng-scope.sh --image j0lama/ng-scope:latest --earfcn "700 300"
+  ./ng-scope.sh --image j0lama/ng-scope:latest --frag --earfcn "700 300"
+  ./ng-scope.sh --image j0lama/ng-scope:latest --frag --earfcn "700 300" --out enb_logs
 ```
 
 # Compile your own NG-Scope image
---
+++ ng-scope.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+Help() {
+    echo "Usage:   $0 <NG-Scope Image name> <EARFCN List>"
+    echo "Example: $0 j0lama/ng-scope:latest 700"
+    echo "Example: $0 j0lama/ng-scope:latest 700 300"
+    echo
+    exit 1
+}
+
+Help() {
+    echo "Usage: $0 [Options...]" >&2
+    echo "  -i, --image <Image name>        Name of the Docker image (e.g. j0lama/ng-scope:latest)"
+    echo "  -o, --out  <Output folder>      Name of the Docker image (e.g. j0lama/ng-scope:latest)"
+    echo "  -f, --frag                      Enable log fragmentation"
+    echo "  -e, --earfcn \"<EARFCN List>\"    List of EARFCN (Use quotes)"
+    echo "  -h, --help                      Show help menu"
+    echo "Examples:"
+    echo "  $0 --image j0lama/ng-scope:latest --earfcn \"700\""
+    echo "  $0 --image j0lama/ng-scope:latest --earfcn \"700 300\""
+    echo "  $0 --image j0lama/ng-scope:latest --frag --earfcn \"700 300\""
+    echo "  $0 --image j0lama/ng-scope:latest --frag --earfcn \"700 300\" --out enb_logs"
+    exit 1
+}
+
+FRAG="no"
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -i|--image)
+      IMAGE="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -f|--frag)
+      FRAG="yes"
+      shift # past argument
+      ;;
+    -e|--earfcn)
+      EARFCN=$2
+      shift # past argument
+      shift # past value
+      ;;
+    -o|--out)
+      OUTPUT=$2
+      shift # past argument
+      shift # past value
+      ;;
+    -h|--help)
+      Help
+      ;;
+    *)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+  esac
+done
+
+# Cheking arguments
+if [[ -z "$IMAGE" ]]; then
+    echo "Error: Image name argument missing"
+    echo "Execute '$0 --help' for more help"
+    exit 1
+fi
+if [[ -z "$EARFCN" ]]; then
+    echo "Error: List of EARFCN missing"
+    echo "Execute '$0 --help' for more help"
+    exit 1
+fi
+
+docker run -e HOST_HOSTNAME=`hostname` --name ng-scope -ti --privileged --rm -v $(pwd):/ng-scope/build/ngscope/src/logs/ $IMAGE ./start.sh $FRAG $(echo "$EARFCN" | tr -d '"')
+
+if [[ ! -z "$OUTPUT" ]]; then
+    mv logs/ $OUTPUT
+fi
