4cdf2bbcf26e48d7826549f31b68467c19f16dda
Alex Marder <amarder@jhu.edu>
2023-09-20 14:57:06 -0400

    install now pulls docker image

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-cd docker || exit
-sudo docker build -t ngscope .
-cd ..
+
+sudo docker pull princetonpaws/ng-scope:22.04
 sudo ln -s $(pwd)/ng-scope.sh /usr/local/bin/ngscope-docker
